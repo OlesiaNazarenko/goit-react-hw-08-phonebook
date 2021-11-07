674c5eb9de177ca4db8d956bcca3929cf1c80c2a
OlesiaNazarenko <eightpa8@gmail.com>
2021-11-07 10:21:55 +0200

    add autocomplete

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -34,7 +34,7 @@ class ContactForm extends Component {
                     type="text"
                     name="name"
                     value={this.state.name}
-                    // autoComplete="off"
+                    autoComplete="off"
                     onChange={this.handleChange}
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
@@ -47,7 +47,7 @@ class ContactForm extends Component {
                     type="tel"
                     name="number"
                     value={this.state.number}
-                    // autoComplete="off"
+                    autoComplete="off"
                     onChange={this.handleChange}
                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                     title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
